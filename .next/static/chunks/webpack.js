2f4779ea0ca0961dff75bec1edea8fce11268d11
docmarcelo <dsrslps@gmail.com>
2021-03-26 13:05:07 -0300

    mudança em landing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f80679acbd18d9fa1baf";
+/******/ 	var hotCurrentHash = "dcd22f689f780129189f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
